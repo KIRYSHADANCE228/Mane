fde1dbf90fe1556992791e1b9794e1d881fdbf86
KIRYSHADANCE228 <62277013+KIRYSHADANCE228@users.noreply.github.com>
2026-06-21 21:29:26 +0700

    Program.cs

--- Program.cs
+++ Program.cs
@@ -2,11 +2,7 @@ using System;
 
 namespace BasicCSharpTasks
 {
-    /// <summary>
-    /// Консольное приложение с решением двух базовых заданий по C#:
-    /// 1. Возведение в степень a^n с использованием только умножения.
-    /// 2. Преобразование числа x в число n путём переноса второй цифры в конец числа.
-    /// </summary>
+
     internal class Program
     {
         private static void Main(string[] args)
@@ -22,11 +18,6 @@ namespace BasicCSharpTasks
             Console.WriteLine("Нажмите любую клавишу для выхода...");
             Console.ReadKey();
         }
-
-        /// <summary>
-        /// Считывает с клавиатуры натуральные числа a и n, вычисляет a^n
-        /// и выводит результат на экран.
-        /// </summary>
         private static void RunTask1()
         {
             int a = ReadNaturalNumber("Введите a (натуральное число): ");
@@ -36,16 +27,6 @@ namespace BasicCSharpTasks
 
             Console.WriteLine($"{a}^{n} = {result}");
         }
-
-        /// <summary>
-        /// Вычисляет a^n, используя в качестве единственной арифметической
-        /// операции над накопленным результатом операцию умножения.
-        /// Счётчик цикла используется только для управления количеством
-        /// итераций и не участвует в вычислении самого значения степени.
-        /// </summary>
-        /// <param name="a">Основание степени (натуральное число).</param>
-        /// <param name="n">Показатель степени (натуральное число).</param>
-        /// <returns>Значение a в степени n.</returns>
         private static long Power(int a, int n)
         {
             long result = 1;
@@ -58,12 +39,7 @@ namespace BasicCSharpTasks
             return result;
         }
 
-        /// <summary>
-        /// Считывает с клавиатуры число x (x >= 100, более двух цифр)
-        /// и вычисляет число n путём:
-        /// 1) удаления второй цифры числа x;
-        /// 2) приписывания этой же цифры в конец полученного числа.
-        /// </summary>
+
         private static void RunTask2()
         {
             long x = ReadNumberWithCondition(
@@ -75,12 +51,6 @@ namespace BasicCSharpTasks
             Console.WriteLine($"x = {x}");
             Console.WriteLine($"n = {n}");
         }
-
-        /// <summary>
-        /// Удаляет вторую цифру числа x и приписывает её в конец числа.
-        /// </summary>
-        /// <param name="x">Исходное число (более двух цифр).</param>
-        /// <returns>Преобразованное число n.</returns>
         private static long TransformBySecondDigit(long x)
         {
             string digits = x.ToString();
@@ -91,13 +61,6 @@ namespace BasicCSharpTasks
 
             return long.Parse(transformed);
         }
-
-        /// <summary>
-        /// Запрашивает у пользователя ввод натурального числа (целое, > 0),
-        /// повторяя запрос до получения корректного значения.
-        /// </summary>
-        /// <param name="prompt">Текст подсказки для пользователя.</param>
-        /// <returns>Введённое натуральное число.</returns>
         private static int ReadNaturalNumber(string prompt)
         {
             int value;
@@ -115,14 +78,6 @@ namespace BasicCSharpTasks
                 Console.WriteLine("Ошибка: введите натуральное число (целое, больше нуля).");
             }
         }
-
-        /// <summary>
-        /// Запрашивает у пользователя ввод числа, удовлетворяющего заданному условию,
-        /// повторяя запрос до получения корректного значения.
-        /// </summary>
-        /// <param name="prompt">Текст подсказки для пользователя.</param>
-        /// <param name="condition">Условие, которое должно выполняться для введённого числа.</param>
-        /// <returns>Введённое число, удовлетворяющее условию.</returns>
         private static long ReadNumberWithCondition(string prompt, Func<long, bool> condition)
         {
             long value;
